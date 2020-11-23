56f401ec1ef5906b40b278b086981f8583da089c
Mehmet Emin Sayım <55194309+MehmetEminSayim@users.noreply.github.com>
2020-11-23 23:08:55 +0300

    Update curlSifreliErisim.php

--- curlSifreliErisim.php
+++ curlSifreliErisim.php
@@ -1,12 +1,12 @@
 <?php
-$bilgi = "ozge:ozge.ozge";
+$bilgi = "kuladisifre";
 $useragent = $_SERVER['HTTP_USER_AGENT'];
 
 $curl = curl_init();
 curl_setopt($curl,CURLOPT_USERAGENT,$useragent);
 curl_setopt($curl,CURLOPT_USERPWD,$bilgi);
-curl_setopt($curl,CURLOPT_REFERER,"http://www.google.com/");
-curl_setopt($curl,CURLOPT_URL, "https://www.ozgeceblog.com/wp-admin/");
+curl_setopt($curl,CURLOPT_REFERER,"url");
+curl_setopt($curl,CURLOPT_URL, "url");
 curl_setopt($curl,CURLOPT_RETURNTRANSFER,true);
 curl_setopt($curl,CURLOPT_FOLLOWLOCATION,true);
 
